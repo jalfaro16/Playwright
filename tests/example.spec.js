0de5b2a106bd1ef646565f5a3aebee74a85c356e
Jason Snow <jason@gmail.com>
2025-03-08 17:51:31 -0600

    Update to example.spec.js

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -4,3 +4,5 @@ test('Verificar que la página de Google carga correctamente', async ({ page }) 
   await page.goto('https://www.google.com');
   await expect(page).toHaveTitle(/Google/);
 });
+
+//This is just a test1 03/082025 549PMCR
